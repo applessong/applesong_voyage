4fd178380a848a626d5755f456d933038818d319
applessong <ok3565z@naver.com>
2022-06-29 18:59:32 +0900

    folder redefinition

--- src/main/java/algorithm/걷기반과제_BUS.java
+++ src/main/java/algorithm/걷기반과제_BUS.java
@@ -13,35 +13,39 @@ class 걷기반과제_BUS {
 
     private static int busNum = 1000;
 
-    // 유일한 인스턴스 생성
-    private static 걷기반과제_BUS instance = new 걷기반과제_BUS();
+//    // 유일한 인스턴스 생성
+//    private static 걷기반과제_BUS instance = new 걷기반과제_BUS();
+//
+//    // 유일한 생성자 구현
+//    private 걷기반과제_BUS() { }
+//
+//    // 외부에서 접근 가능한 getter 메서드 구현
+//    public static 걷기반과제_BUS getInstance() {
+//        if( instance == null) {
+//            instance = new 걷기반과제_BUS();
+//        }
+//        return instance;
+//    }
+//
+//    // 버스를 만들어주는 메서드
+//    // Car 객체를 데이터타입으로 지정해주어서 해당 메서드에서 인스턴스가 생성되게 함
+//
+//    public 걷기반과제_BUS createbus() {
+//        걷기반과제_BUS bus = new 걷기반과제_BUS();
+//        return bus; // 반환값은 인스턴스를 반환할 수 있도록 함
+//    }
 
-    // 유일한 생성자 구현
-    private 걷기반과제_BUS() { }
 
-    // 외부에서 접근 가능한 getter 메서드 구현
-    public static 걷기반과제_BUS getInstance() {
-        if( instance == null) {
-            instance = new 걷기반과제_BUS();
-        }
-        return instance;
-    }
-
-    // 차를 만들어주는 메서드
-    // Car 객체를 데이터타입으로 지정해주어서 해당 메서드에서 인스턴스가 생성되게 함
-
-    public 걷기반과제_BUS createbus() {
-        걷기반과제_BUS bus = new 걷기반과제_BUS();
-        return bus; // 반환값은 인스턴스를 반환할 수 있도록 함
-    }
-    // 차 고유번호 생성 메서드
-    // 인스턴스가 새로 생성될 때마다 static 변수 carnum의 값이 1씩 상승함
+//  버스 고유번호 생성 메서드 시작//
+// 인스턴스가 새로 생성될 때마다 static 변수 carnum의 값이 1씩 상승함
     public int getBusNum() {
         busNum++;
         return busNum;
     }
+//  버스 고유번호 생성 메서드 종료//
 
-//   기름 양 확인 후 운행여부 실행 메소드 시작//
+
+//  기름 양 확인 후 운행여부 실행 메소드 시작//
     public boolean now_fuel (int fuel) {
         this.operate = true;
         if ( this.fuel <= 0 ){
@@ -50,7 +54,8 @@ class 걷기반과제_BUS {
         System.out.println(this.operateSituation(this.operate, this.fuel));
         return this.operate;
     }
-//   기름 양 확인 후 운행여부 확인 메소드 종료//
+//  기름 양 확인 후 운행여부 확인 메소드 종료//
+
 
 //  운행 여부와 연료량 부족메세지송출 메소드 시작//
     public String operateSituation(boolean operate, int fuel) {
@@ -80,11 +85,16 @@ class 걷기반과제_BUS {
         return this.passneger;
     }
 //  승객탑승 메서드 종료//
-//    탑승 승객 수 확인 메서드 시작//
+
+
+//  탑승 승객 수 확인 메서드 시작//
     public int checkPassenger(){
         System.out.println("현재 승객 수는 "+this.passneger+"입니다.");
         return this.passneger;
     }
+//  탑승 승객 수 확인 메서드 종료//
+
+
 //  속도변경 메서드 시작//
     public int transeSpeed(){
         if ( this.operate == true) {
@@ -99,7 +109,7 @@ class 걷기반과제_BUS {
         System.out.println("현재 운행 속도는"+this.speed+"입니다.");
         return this.speed;
     }
-
+//  속도변경 메서드 종료//
 
 
 
@@ -118,8 +128,8 @@ class 걷기반과제_BUS {
 //        System.out.printf("주행하실 속도를 입력해주세요  >>  ");
         bus.transeSpeed();
         bus.CheckSpeed();
-
         System.out.println();
+
         걷기반과제_BUS bus1 = new 걷기반과제_BUS();
         bus1.getBusNum();
         System.out.println("버스 번호 : "+bus1.busNum);
